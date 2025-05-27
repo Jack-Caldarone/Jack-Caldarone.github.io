5833980c152614f390169602536c1092f33c0a2f
Jack-Caldarone <148284496+Jack-Caldarone@users.noreply.github.com>
2025-05-27 15:56:19 -0400

    logos, icon, fixing css

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import picOfMe from '/placeholder.jpg'
+import githubLogo from '/github.svg'
+import linkedinLogo from '/linkedin.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='background'>
+    <div>
         <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
+          <a href="https://github.com/Jack-Caldarone" target="_blank">
+            <img src={githubLogo} className="logo" alt="Vite logo" />
           </a>
           <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
+            <img src={linkedinLogo} className="logo" alt="Linkedin logo" />
           </a>
         </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
+        <h1>Hi, I'm Jack C </h1>
+        <h1>\ (•◡•) /</h1>
+        <img src={picOfMe}/>
     </div>
   )
 }
